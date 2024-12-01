6600728d5061a42dbb14c9d6a8e46b461ca527a7
Kinamura <airfighter001@users.noreply.github.com>
2024-12-01 09:43:34 +0100

    Increase readability of solution

--- 2024/Haskell/Day-01/Day-01.hs
+++ 2024/Haskell/Day-01/Day-01.hs
@@ -1,11 +1,11 @@
 import Data.List (length, map, sort)
 
 parseInput :: String -> ([Int], [Int])
-parseInput content = (unzip . map (\[x, y] -> (x, y)) . map (map read . words) . lines) content
+parseInput= unzip . map ((\[x, y] -> (x, y)) . map read . words) . lines
 
 main :: IO ()
 main = do
     content <- readFile "01input.txt"
     let (first, second) = parseInput content
-    print (foldr1 (+) $ map (abs . uncurry (-)) $ zip (sort first) (sort second))
-    print (foldr1 (+) $ map (\x -> (length $ filter (x==) second) * x ) first)
+    print (sum $ map (abs . uncurry (-)) $ zip (sort first) (sort second))
+    print (sum $ map (\x -> length (filter (x==) second) * x ) first)
