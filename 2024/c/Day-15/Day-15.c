52fdf9cf724814c6deb197b19434c15fdeb91329
kinamura <julian@dreuth.de>
2024-12-16 15:21:05 +0100

    Day-15 in C

--- 2024/c/Day-15/Day-15.c
+++ 2024/c/Day-15/Day-15.c
@@ -1,123 +1,138 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-#define LINE_SIZE 5000
-#define LINES 80
+#define SIZE 1024
+int startRow = -1, rows = 0, currX, currY, robotX, robotY;
 
-int readInput(char prompts[][LINE_SIZE]){
-	FILE *input = fopen("./15input.txt", "r");
-	int i = 0;
-	if (input != NULL){
-		while (fgets(prompts[i++],LINE_SIZE,input) != NULL) {}
-	}
-	return (fclose(input) & 0) | (i - 1);
+void move(char grid[SIZE][SIZE], char c) {
+    int dirX = (c == '>') - (c == '<');
+    int dirY = (c == 'v') - (c == '^');
+    int tempX = currX + dirX, tempY = currY + dirY;
+    while (grid[tempY][tempX] == 'O') {
+        tempY += dirY;
+        tempX += dirX;
+    }
+    if (grid[tempY][tempX] == '#') return;
+    while (tempY != currY || tempX != currX) {
+        grid[tempY][tempX] = grid[tempY - dirY][tempX - dirX];
+        tempY = tempY - dirY;
+        tempX = tempX - dirX;
+    }
+    currY += dirY;
+    currX += dirX;
 }
 
-int parseInput(int lines, char input[][LINE_SIZE], int grid[][LINES], int botPos[2]) {
-  int movementStart = 0, isGrid = 1;
-  while(isGrid) {
-    if(strlen(input[movementStart]) == 1) break;
-    int i = 0;
-    while(input[movementStart][i] != '\n') {
-      if (input[movementStart][i] == '@') {
-        botPos[0] = movementStart;
-        botPos[1] = i;
-      }
-      grid[movementStart][i] = input[movementStart][i];
-      i++;
-    }
-    i = 0;
-    movementStart++;
-  }
-  return movementStart + 1;
+int checkVert(char grid2[SIZE][SIZE], int x, int y, int dirY) {
+    if (grid2[y + dirY][x] == '#') return 1;
+    if (grid2[y + dirY][x] == '.') return 0;
+    return checkVert(grid2, x, y + dirY, dirY) + checkVert(grid2, x + (grid2[y + dirY][x] == '[' ? 1 : -1), y + dirY, dirY);
 }
 
-void printGrid(int grid[][LINES]) {
-  for(int i = 0; i < LINES/2; i++) {
-    for(int j = 0; j < LINES/2; j++) {
-      printf("%c", grid[i][j]);
+void moveVert(char grid2[SIZE][SIZE], int x, int y, int dirY) {
+    if (grid2[y + dirY][x] == '[' || grid2[y + dirY][x] == ']') {
+        moveVert(grid2, x + ((grid2[y + dirY][x] == '[') ? 1 : -1), y + dirY, dirY);
+        moveVert(grid2, x, y + dirY, dirY);
     }
-    printf("\n");
-  }
-
+    grid2[y + dirY][x] = grid2[y][x];
+    if (grid2[y][x] == '@') robotY = y + dirY;
+    grid2[y][x] = '.';
 }
 
-void tryPushStones(int grid[][LINES], int botPos[2], int dir[2]) {
-  int stonePos[2] = {botPos[0] + dir[0], botPos[1] + dir[1]};
-  if(grid[stonePos[0]][stonePos[1]] == '.') return;
-  if(grid[stonePos[0] + dir[0]][stonePos[1] + dir[1]] == 'O') {
-    tryPushStones(grid, stonePos, dir);
-  }
-  if(grid[stonePos[0] + dir[0]][stonePos[1] + dir[1]] == '#') return;
-  if(grid[stonePos[0] + dir[0]][stonePos[1] + dir[1]] == '.') {
-    grid[stonePos[0] + dir[0]][stonePos[1] + dir[1]] = 'O';
-    grid[stonePos[0]][stonePos[1]] = '.';
-  }
+void move2(char grid2[SIZE][SIZE], int x, int y, char c) {
+    int dirX = (c == '>') - (c == '<');
+    int dirY = (c == 'v') - (c == '^');
+    int tempX;
+    if (dirY == 0) {
+        tempX = x;
+        while (grid2[y][tempX + dirX] == '[' || grid2[y][tempX + dirX] == ']') tempX += dirX;
+        if (grid2[y][tempX + dirX] == '#') return;
+        while (tempX != x) {
+            grid2[y][tempX + dirX] = grid2[y][tempX];
+            tempX -= dirX;
+        }
+        grid2[y][x + dirX] = grid2[y][x];
+        robotX = x + dirX;
+        grid2[y][x] = '.';
+    } else {
+        if (checkVert(grid2, x, y, dirY) == 0) moveVert(grid2, x, y, dirY);
+    }
 }
 
-void moveRobot(int start, int grid[][LINES], char input[][LINE_SIZE], int lines, int botPos[2]) {
-  int dir[4][2] = {{1,0}, {0,1}, {-1,0}, {0,-1}}, pos = 0, currDir = -1;
-  while(start < lines) {
-    if(input[start][pos] == '\n') {
-      pos = 0;
-      start++;
-      continue;
+void smallWarehouse(char grid[SIZE][SIZE]) {
+    FILE * fp = fopen("./15input.txt", "r");
+    char * p;
+    while (fscanf(fp, "%s", grid[rows]) == 1) {
+        if ((p = strchr(grid[rows], '@'))) {
+            currX = p - grid[rows];
+            currY = rows;
+        }
+        if (strchr(grid[rows], '<') && startRow == -1) startRow = rows;
+        rows++;
     }
-    if(input[start][pos] == '^') currDir = 2;
-    if(input[start][pos] == 'v') currDir = 0;
-    if(input[start][pos] == '>') currDir = 1;
-    if(input[start][pos] == '<') currDir = 3;
-    if(grid[botPos[0]+dir[currDir][0]][botPos[1]+dir[currDir][1]] == '#') {
-      pos++;
-      continue;
+    fclose(fp);
+    for (int i = startRow; i < rows; i++) {
+        for (int j = 0; j < (int) strlen(grid[i]); j++) {
+            move(grid, grid[i][j]);
+        }
     }
-    if(grid[botPos[0]+dir[currDir][0]][botPos[1]+dir[currDir][1]] == '.') {
-      grid[botPos[0]][botPos[1]] = '.';
-      botPos[0] = botPos[0] + dir[currDir][0];
-      botPos[1] = botPos[1] + dir[currDir][1];
-      grid[botPos[0]][botPos[1]] = '@';
-      pos++;
-      continue;
-    }
-    tryPushStones(grid, botPos, dir[currDir]);
-    if(grid[botPos[0]+dir[currDir][0]][botPos[1]+dir[currDir][1]] == '.') {
-      grid[botPos[0]][botPos[1]] = '.';
-      botPos[0] = botPos[0] + dir[currDir][0];
-      botPos[1] = botPos[1] + dir[currDir][1];
-      grid[botPos[0]][botPos[1]] = '@';
-      pos++;
-      continue;
-    }
-    pos++;
-    //printGrid(grid);
-  }
 }
 
-int sumGPS(int grid[][LINES]) {
-  int sum = 0;
-  for(int i = 0; i < LINES; i++) {
-    for(int j = 0; j < LINES; j++) {
-      //printf("%d %d %d\n", sum, grid[i][j], 'O');
-      if(grid[i][j] == 'O') sum += (100 * i + j);
+void bigWarehouse(char grid2[SIZE][SIZE]) {
+    FILE * fp = fopen("./15input.txt", "r");
+    int d;
+    char buff[SIZE];
+    while (fscanf(fp, "%s", buff) == 1) {
+        if (strchr(buff, '<') && startRow == -1) startRow = rows;
+        if (startRow == -1) {
+            d = 0;
+            for (int i = 0; i < (int)strlen(buff); i++) {
+                if (buff[i] == '@') {
+                    robotX = d;
+                    robotY = rows;
+                    grid2[rows][d++] = '@';
+                    grid2[rows][d++] = '.';
+                } else if (buff[i] == 'O') {
+                    grid2[rows][d++] = '[';
+                    grid2[rows][d++] = ']';
+                } else {
+                    grid2[rows][d++] = buff[i];
+                    grid2[rows][d++] = buff[i];
+                }
+            }
+            grid2[rows][d] = '\0';
+        } else {
+            strcpy(grid2[rows], buff);
+        }
+        rows++;
     }
-  }
-  return sum;
+    fclose(fp);
+    for (int i = startRow; i < rows; i++) {
+        for (int j = 0; j < (int)strlen(grid2[i]); j++) {
+            move2(grid2, robotX, robotY, grid2[i][j]);
+        }
+    }
 }
 
-int main(int argc, char** argv) {
-	char input[LINES][LINE_SIZE];
-  int botPos[2];
-	int lineCount = readInput(input);
-	//for (int i = 0; i < lineCount; i++) {
-	//	fprintf(stdout, "%d %s",i, input[i]);
-	//}
-  int grid[LINES][LINES];
-  int start = parseInput(lineCount, input, grid, botPos);
-  //printf("%d \n", start);
-  //printf("%d %d\n", botPos[0], botPos[1]);
-  int dir[2] = {0,-1};
-  moveRobot(start, grid, input, lineCount, botPos);
-  int sum = sumGPS(grid);
-  printf("%d\n", sum);
-	return 0;
+int main(int argc, char ** argv) {
+    char grid[SIZE][SIZE], grid2[SIZE][SIZE];
+    smallWarehouse(grid);
+    int sum = 0;
+    for (int i = 0; i < startRow; i++) {
+        for (int j = 0; j < (int)strlen(grid[i]); j++) {
+            if (grid[i][j] == 'O') sum += 100 * i + j;
+        }
+    }
+    printf("%d\n", sum);
+    startRow = -1;
+    rows = 0;
+    bigWarehouse(grid2);
+    sum = 0;
+    for (int i = 0; i < startRow; i++) {
+        for (int j = 0; j < (int) strlen(grid2[i]); j++) {
+            if (grid2[i][j] == '[') sum += 100 * i + j;
+        }
+    }
+    printf("%d\n", sum);
+    return 0;
 }
