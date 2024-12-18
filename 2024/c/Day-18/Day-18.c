08164e24b5afb54bae0179191850786df741a3e4
kinamura <julian@dreuth.de>
2024-12-18 07:56:02 +0100

    Day-18 in C Part 2

--- 2024/c/Day-18/Day-18.c
+++ 2024/c/Day-18/Day-18.c
@@ -4,6 +4,7 @@
 #define LINE_SIZE 32
 #define LINES 3450
 #define DIM 71
+#define P1 1024
 
 int readInput(char prompts[][LINE_SIZE]){
 	FILE *input = fopen("./18input.txt", "r");
@@ -16,9 +17,8 @@ int readInput(char prompts[][LINE_SIZE]){
 
 void parseInput(char input[][LINE_SIZE], char grid[DIM][DIM]) {
     int x = 0 ,y = 0;
-	for (int i = 0; i < 1024; i++) {
+	for (int i = 0; i < P1; i++) {
 		sscanf(input[i], "%d,%d", &y, &x);
-        //printf("%d,%d\n", y, x);
         grid[x][y] = '#';
     }
 }
@@ -33,19 +33,41 @@ void findPath(char grid[DIM][DIM], int costmap[DIM][DIM], int x, int y, int cost
 	findPath(grid,costmap,x,y-1,cost + 1);
 }
 
+void findBlockCoords(char grid[DIM][DIM], char input[][LINE_SIZE]) {
+    int i = P1;
+	while (1) {
+		int x = 0 ,y = 0;
+		sscanf(input[i++], "%d,%d", &y, &x);
+		grid[x][y] = '#';
+        int costmap[DIM][DIM];
+		for (int a = 0; a < DIM; a++) {
+			for (int b = 0; b < DIM; b++) {
+				costmap[a][b] = 99999999;
+			}
+		}
+		costmap[0][0] = 1;
+		findPath(grid, costmap, 0, 0, 0);
+        if (costmap[DIM-1][DIM-1] == 99999999) {
+        	printf("First blocking byte: (%d,%d)\n", y,x);
+            return;
+        }
+    }
+}
+
 int main(int argc, char** argv) {
 	char input[LINES][LINE_SIZE], grid[DIM][DIM];
     int costmap[DIM][DIM] = {0};
 	int lineCount = readInput(input);
     for (int i = 0; i < DIM; i++) {
-      for (int j = 0; j < DIM; j++) {
-        grid[i][j] = '.';
-        costmap[i][j] = 99999999;
-      }
+        for (int j = 0; j < DIM; j++) {
+            grid[i][j] = '.';
+            costmap[i][j] = 99999999;
+        }
     }
     parseInput(input, grid);
     costmap[0][0] = 1;
     findPath(grid, costmap, 0, 0, 0);
-    printf("Part 1: %d\n", costmap[DIM-1][DIM-1]);
+    printf("Minimum steps: %d\n", costmap[DIM-1][DIM-1]);
+    findBlockCoords(grid, input);
 	return 0;
 }
