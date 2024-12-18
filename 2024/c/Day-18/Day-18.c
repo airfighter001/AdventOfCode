715ee13a12de8465c4883c980d142fff9cf01fec
kinamura <julian@dreuth.de>
2024-12-18 08:13:49 +0100

    Day-18 in C slight changes

--- 2024/c/Day-18/Day-18.c
+++ 2024/c/Day-18/Day-18.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LINE_SIZE 32
 #define LINES 3450
-#define DIM 71
-#define P1 1024
 
-int readInput(char prompts[][LINE_SIZE]){
+int readInput(char prompts[][32]){
 	FILE *input = fopen("./18input.txt", "r");
 	int i = 0;
 	if (input != NULL){
-		while (fgets(prompts[i++],LINE_SIZE,input) != NULL) {}
+		while (fgets(prompts[i++],32,input) != NULL) {}
 	}
 	return (fclose(input) & 0) | (i - 1);
 }
 
-void parseInput(char input[][LINE_SIZE], char grid[DIM][DIM]) {
+void parseInput(char input[][32], char grid[71][71]) {
     int x = 0 ,y = 0;
-	for (int i = 0; i < P1; i++) {
+	for (int i = 0; i < 1024; i++) {
 		sscanf(input[i], "%d,%d", &y, &x);
         grid[x][y] = '#';
     }
 }
 
-void findPath(char grid[DIM][DIM], int costmap[DIM][DIM], int x, int y, int cost) {
-	if(x < 0 || x >= DIM || y < 0 || y >= DIM) return;
+void findPath(char grid[71][71], int costmap[71][71], int x, int y, int cost) {
+	if(x < 0 || x >= 71 || y < 0 || y >= 71) return;
     if(costmap[x][y] <= cost || grid[x][y] == '#') return;
     costmap[x][y] = cost;
     findPath(grid,costmap,x+1,y,cost + 1);
@@ -33,21 +30,24 @@ void findPath(char grid[DIM][DIM], int costmap[DIM][DIM], int x, int y, int cost
 	findPath(grid,costmap,x,y-1,cost + 1);
 }
 
-void findBlockCoords(char grid[DIM][DIM], char input[][LINE_SIZE]) {
-    int i = P1;
+void initCostmap(int costmap[71][71]) {
+	for (int a = 0; a < 71; a++) {
+		for (int b = 0; b < 71; b++) {
+			costmap[a][b] = 99999999;
+		}
+	}
+}
+
+void findBlockCoords(char grid[71][71], char input[][32]) {
+    int i = 1024;
 	while (1) {
-		int x = 0 ,y = 0;
+		int x = 0 ,y = 0, costmap[71][71];
 		sscanf(input[i++], "%d,%d", &y, &x);
 		grid[x][y] = '#';
-        int costmap[DIM][DIM];
-		for (int a = 0; a < DIM; a++) {
-			for (int b = 0; b < DIM; b++) {
-				costmap[a][b] = 99999999;
-			}
-		}
+        initCostmap(costmap);
 		costmap[0][0] = 1;
 		findPath(grid, costmap, 0, 0, 0);
-        if (costmap[DIM-1][DIM-1] == 99999999) {
+        if (costmap[71-1][71-1] == 99999999) {
         	printf("First blocking byte: (%d,%d)\n", y,x);
             return;
         }
@@ -55,19 +55,13 @@ void findBlockCoords(char grid[DIM][DIM], char input[][LINE_SIZE]) {
 }
 
 int main(int argc, char** argv) {
-	char input[LINES][LINE_SIZE], grid[DIM][DIM];
-    int costmap[DIM][DIM] = {0};
-	int lineCount = readInput(input);
-    for (int i = 0; i < DIM; i++) {
-        for (int j = 0; j < DIM; j++) {
-            grid[i][j] = '.';
-            costmap[i][j] = 99999999;
-        }
-    }
+	char input[LINES][32], grid[71][71];
+    int costmap[71][71], lineCount = readInput(input);;
+    initCostmap(costmap);
     parseInput(input, grid);
     costmap[0][0] = 1;
     findPath(grid, costmap, 0, 0, 0);
-    printf("Minimum steps: %d\n", costmap[DIM-1][DIM-1]);
+    printf("Minimum steps: %d\n", costmap[71-1][71-1]);
     findBlockCoords(grid, input);
 	return 0;
 }
