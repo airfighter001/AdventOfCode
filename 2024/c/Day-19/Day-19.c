caeaa3c009ca1c26f22d2fd1f9bef116d25cde6d
kinamura <julian@dreuth.de>
2024-12-19 11:06:13 +0100

    Day-19 in C Part 2

--- 2024/c/Day-19/Day-19.c
+++ 2024/c/Day-19/Day-19.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-#define LINE_SIZE 3000
-#define LINES 500
-
-int readInput(char prompts[][LINE_SIZE]){
+int readInput(char prompts[][3000]){
 	FILE *input = fopen("./19input.txt", "r");
 	int i = 0;
 	if (input != NULL){
-		while (fgets(prompts[i++],LINE_SIZE,input) != NULL) {}
+		while (fgets(prompts[i++],3000,input) != NULL) {}
 	}
 	return (fclose(input) & 0) | (i - 1);
 }
 
-void parseInput(char inputs[][LINE_SIZE], char exPats[][20], char desPats[][100]) {
+int parseInput(char inputs[][3000], char exPats[][20], char desPats[][100]) {
 	int i = 0, exPatCount = 0, desPatCount = 0;
-	char *token = strtok(inputs[i], ",");
+	char *token = strtok(inputs[i++], ",");
 	while (token != NULL) {
 		char filteredToken[20] = {0};
 		int k = 0;
@@ -25,54 +22,49 @@ void parseInput(char inputs[][LINE_SIZE], char exPats[][20], char desPats[][100]
 			}
 		}
 		filteredToken[k] = '\0';
-		strncpy(exPats[exPatCount], filteredToken, 19);
-		exPats[exPatCount][19] = '\0';
-		exPatCount++;
+		strncpy(exPats[exPatCount], filteredToken, 20);
+		exPats[exPatCount++][19] = '\0';
 		token = strtok(NULL, ",");
 	}
-	i++;
+    exPats[exPatCount - 1][strlen(exPats[exPatCount - 1]) -1] = '\0';
 	while (inputs[++i][0] != '\0') {
 		strncpy(desPats[desPatCount], inputs[i], 99);
-		desPats[desPatCount][strlen(inputs[i]) -1] = '\0';
-		desPatCount++;
+		desPats[desPatCount++][strlen(inputs[i]) -1] = '\0';
 	}
+    return exPatCount;
 }
 
-int findMatchingPatternsRecursive(const char *desPat, char exPats[][20], int exPatCount, int position) {
-	int desPatLen = strlen(desPat);
-	if (position == desPatLen) {
-		return 1;
-	}
-	for (int j = 0; j < exPatCount; j++) {
-		int exPatLen = strlen(exPats[j]);
-		if (exPatLen > 0 && position + exPatLen <= desPatLen && strncmp(&desPat[position], exPats[j], exPatLen) == 0) {
-			if (findMatchingPatternsRecursive(desPat, exPats, exPatCount, position + exPatLen)) {
-				return 1;
-			}
+long checkPats(char* desPat, int currChar, long lut[100], char exPats[][20], int numTowels) {
+	if (lut[currChar] != -1) return lut[currChar];
+	long sum = 0;
+	char* subDesPat = desPat + currChar;
+	int sl = strlen(subDesPat);
+	for (int i = 0; i < numTowels; i++) {
+		int exPatLen = strlen(exPats[i]);
+		if (exPatLen == sl && strcmp(subDesPat, exPats[i]) == 0) sum ++;
+		if (exPatLen < sl && strncmp(subDesPat, exPats[i], exPatLen) == 0) {
+			sum += checkPats(desPat, currChar + exPatLen, lut, exPats, numTowels);
 		}
 	}
-	return 0;
-}
-
-int findMatchingPatterns(const char *desPat, char exPats[][20], int exPatCount) {
-	if (findMatchingPatternsRecursive(desPat, exPats, exPatCount, 0)) {
-		return 1;
-	}
-	return 0;
+	lut[currChar] = sum;
+	return sum;
 }
 
 int main() {
-	char input[LINES][LINE_SIZE] = {0};
-	char exPats[LINES][20] = {0};
-	char desPats[LINES][100] = {0};
-	int lineCount = readInput(input);
-	parseInput(input, exPats, desPats);
-	int matchCount = 0;
-	for (int i = 0; i < LINES && desPats[i][0] != '\0'; i++) {
-		if (findMatchingPatterns(desPats[i], exPats, LINES)) {
-			matchCount++;
-		}
-	}
-	printf("Possible designs: %d\n", matchCount);
+	char input[500][3000] = {0}, exPats[500][20] = {0}, desPats[500][100] = {0};
+	int lineCount = readInput(input), towelCount = parseInput(input, exPats, desPats), matchCount = 0;
+	long sum = 0;
+    for (int i = 0; i < 500 && desPats[i][0] != '\0'; i++) {
+    	long lut[100];
+    	for (int j = 0; j < 100; j++) {
+    		lut[j] = -1;
+    	}
+    	long combinations = checkPats(desPats[i], 0, lut, exPats, towelCount);
+    	if (combinations != 0) {
+            sum += combinations;
+            matchCount++;
+        }
+    }
+	printf("Possible designs: %d\nPossible combinations: %ld\n", matchCount, sum);
 	return 0;
 }
